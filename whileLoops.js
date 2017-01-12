ca8cea11e29058f4061d683a62b847dae1a1371d
George Higbie <gehigbie@gmail.com>
2017-01-12 01:00:59 -0700

    fixed loop2

--- whileLoops.js
+++ whileLoops.js
@@ -6,11 +6,25 @@ while(num < 19){
 	num++;
 }
 
-var num1 = 10
+var num1 = 10;
 while(num1 < 40){
+	if(num1 % 2 === 0){
 	console.log(num1);
-	num++;
+	}
+	num1++;
 }
+
+var num3 = 300;
+while(num3 < 333 && num3 % 2 === 1){
+	console.log(num3);
+	num++
+}
+
+var num4 = 4;
+while((num4 < 50) && (num4 % 5 === 0) && (num4 % 3 === 0)){
+	console.log(num4);
+}
+
 
 
 
