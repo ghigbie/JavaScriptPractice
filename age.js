f700670c2d25379d4fee3bc425a4c4db5162c661
George Higbie <gehigbie@gmail.com>
2017-01-11 20:35:07 -0700

    added rough calculation for leap years

--- age.js
+++ age.js
@@ -1,8 +1,8 @@
 var ageYears = prompt("Please enter your age to find out how long you've been alive.");
 
-var ageDays = ageYears*365;
+var ageDays = ageYears*365.25;
 //ES6 syntax
 
-alert(`You've been alive for ${ageDays}`);
+alert(`You've been alive for roughly ${ageDays} days`);
 
-console.log(`You've been alive for ${ageDays} days`);
+console.log(`You've been alive for roughly ${ageDays} days`);
