4dadae350f9db9cbeebba8baa6c08cdb63384e89
George Higbie <gehigbie@gmail.com>
2017-01-11 21:16:18 -0700

    added sqareroot function

--- conditionals.js
+++ conditionals.js
@@ -6,9 +6,15 @@ if(age < 0){
 	alert("You cannot possible be that young!");
 	age = prompt(ageAsk);
 }
-else if(age === 21){
+
+if(age === 21){
 	alert("Happy 21st birthday!");
 }
-else if(age % 2 !== 0){
+
+if(age % 2 !== 0){
 	alert("Your age is an odd number!");
+}
+
+if(age % Math.sqrt(age) === 0) {
+	alert("Your age is a perfect square!");
 }
