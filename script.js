07b3d973a311ad7345fb6ffaafd732081700f5ec
George Higbie <gehigbie@gmail.com>
2017-01-11 17:34:12 -0700

    made edits

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 alert("Hello from the JS File!");
 
 var userName = prompt("What is your name?");
-var greeting = "Nice to meet you ";
+var greeting = "Nice to meet you, ";
 
 alert(greeting + userName);
 console.log(greeting + userName);
