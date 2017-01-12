b6f26f7c6f7c28033fe8dc48621018df5287b199
George Higbie <gehigbie@gmail.com>
2017-01-11 21:31:37 -0700

    added function

---
+++ guessingGame.js
@@ -0,0 +1,22 @@
+var magicNumber = 20;
+var enteredNumber;
+runGame();
+
+function runGame(){
+	enteredNumber = prompt("Guess my magic number. Please choose a number.");
+
+	if(enteredNumber < magicNumber){
+		alert(`Please choose a number that is greater tham ${enteredNumber}`);
+		runGame();
+	}
+	else if(enteredNumber > magicNumber){
+		alert(`Please choose a number that is less tham ${enteredumber}`);
+		runGame();
+	}
+	else{
+		alert(`You guessed the magic number of ${enteredNumber}!!!`);
+	}
+
+}
+
+
