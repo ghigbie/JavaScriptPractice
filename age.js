a619f0f5b9c8856c4e3c491b5968b045bfbfdaec
George Higbie <gehigbie@gmail.com>
2017-01-11 20:33:05 -0700

    changed file org

--- age.js
+++ age.js
@@ -3,4 +3,6 @@ var ageYears = prompt("Please enter your age to find out how long you've been al
 var ageDays = ageYears*365;
 //ES6 syntax
 
+alert(`You've been alive for ${ageDays}`);
+
 console.log(`You've been alive for ${ageDays} days`);
