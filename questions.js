1a94b42a1361aa2e397dcc9742250faeae5cea7f
George Higbie <gehigbie@gmail.com>
2017-01-11 17:44:10 -0700

    added js

---
+++ questions.js
@@ -0,0 +1,10 @@
+
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+var nameSentence = "Your full name is " + firstName + " " + lastName + ".";
+var ageSentence = "You are " + age + " " + "years old.";
+
+console.log(nameSentence);
+console.log(ageSentence;
