dbff414c4d15c953815cc820b44ab2c40b8b5614
George Higbie <gehigbie@gmail.com>
2017-01-12 19:35:22 -0700

    added another loop

--- for.js
+++ for.js
@@ -1,10 +1,21 @@
 //created in terminal
 
-for(var a = -10; a < 19; a++){
+for(var a = -10; a <= 19; a++){
 	console.log(a);
 }
 
-for(var b = 10; b < 40; b+=2){
+console.log("/////////////////////");
+
+for(var b = 10; b <= 40; b+=2){
 	console.log(b);
 }
 
+console.log("/////////////////////");
+
+for(var c = 300; c <= 333; c++){
+	if(c % 2 !== 0){
+		console.log(c);
+	}
+}
+
+console.log("/////////////////////");
